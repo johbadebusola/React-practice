070226ae67ca941cbc3a6fd9b65abe61f67c8123
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2022-08-10 18:16:52 +0000

    added <form /> to app.jsx

--- src/EvenNumbers.jsx
+++ src/EvenNumbers.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Even() {
 	let even = 0
 	let num = [21, 23, 6, 7, 9, 54, 12, 6, 4, 8, 34,]
-	let nums = num.map(number =>  {if (number % 2 >= 1) {
+	let nums = num.map(number => {if (number % 2 >= 1) {
 		even++
 	} })
 return (
